288b77cbcc626b18a99d8675b1ee7b143c685d20
Barnatt Wu <barnattwu@gmail.com>
2026-04-01 10:28:29 -0400

    initial commit

---
+++ src/test/java/com/barnattwu/rosetta/RosettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barnattwu.rosetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
